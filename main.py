1c96d1b2a17443b7dd1ba271f38f24c13b5a5175
Rishika Snehi <snehirishika@gmail.com>
2025-11-28 23:51:24 +0530

    created chat function

--- main.py
+++ main.py
@@ -3,30 +3,34 @@ from google import genai
 
 
 api_key = os.getenv("GEMINI_API_KEY")
-
-# print("API loaded")
+print("API loaded")
 
 client = genai.Client()
+MODEL = "gemini-2.5-flash"
+TEMPERATURE = 0.7
+SYSTEM_PROMPT = "You are a cute little bubbly assistant who always speaks with an encouraging and positive tone."
 
-# SYSTEM_INSTRUCTION = "You are a cute little bubbly assistant who always speaks with an encouraging and positive tone."
-
-response = client.models.generate_content(
-    model = "gemini-2.5-flash",
+def chat(user_input):
+    response = client.models.generate_content(
+        model = MODEL,
         contents = [
-            {"role" : "user", "parts" : [{"text" : "Tell me a joke about dogs"}]}
-    ],
-    config = genai.types.GenerateContentConfig(
-        temperature = 0.7,
-        # max_output_tokens = 150,
-        system_instruction = "You are a cute little bubbly assistant who always speaks with an encouraging and positive tone."
-
+            {"role" : "user", "parts" : [{"text" : user_input}]}
+        ],
+        config = genai.types.GenerateContentConfig(
+            temperature = TEMPERATURE,
+            system_instruction = SYSTEM_PROMPT
+        )
     )
-)
-reply = response.text
-if reply is None or reply == "":
-    print("\n--- ⚠️ API Response Failed ---")
-    print(f"Text output (reply): {reply}")
-    print("\nPrompt Feedback (Reason for Failure):")
-    # This will print the detailed reason for the block/failure
-    print(response.prompt_feedback)
-print(reply)
+
+    reply = response.text
+
+    if reply is None or reply == "":
+        print("\n--- ⚠️ API Response Failed ---")
+        print(f"Text output (reply): {reply}")
+        print("\nPrompt Feedback (Reason for Failure):")
+        # This will print the detailed reason for the block/failure
+        print(response.prompt_feedback)
+
+    return reply    
+
+print(chat("Hello, how are you today?"))
