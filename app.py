0ed44853d9bb7a3e9c9b8c80b1f60e65714a6b21
Rishika Snehi <snehirishika@gmail.com>
2025-12-02 17:59:20 +0530

    configuration and state setup completed

--- app.py
+++ app.py
@@ -1,2 +1,25 @@
 import streamlit as st
-import from main import chat, total_token_used, contents, enforce_token_budget, TOKEN_BUDGET
+import os
+from PIL import Image
+from io import BytesIO
+from google.genai.errors import APIError
+
+from main import (
+    chat, 
+    total_token_used, 
+    enforce_token_budget, 
+    MODEL, 
+    INITIAL_SYSTEM_PROMPT
+)
+ 
+st.set_page_config(page_title = "Chatbot", page_icon = "🤖", layout = "wide")
+st.title("💖 Bubbly Gemini Chatbot")
+
+if 'history' not in st.session_state:
+    st.session_state['history'] = []
+if 'system_prompt' not in st.session_state:
+    st.session_state['system_prompt'] = INITIAL_SYSTEM_PROMPT
+if 'token_budget' not in st.session_state:
+    st.session_state['token_budget'] = 4096
+
+
